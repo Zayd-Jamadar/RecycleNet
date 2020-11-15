ee98ac60e71575b88e1bebf714a6d5a17c810380
Harsh188 <hiharshith18@gmail.com>
2020-11-15 13:03:46 +0530

    train and test datasets

--- RecycleNet/moveFiles.py
+++ RecycleNet/moveFiles.py
@@ -7,46 +7,71 @@ metal = os.listdir('dataset/metal')
 paper = os.listdir('dataset/paper')
 plastic = os.listdir('dataset/plastic')
 
-print('Cardboard: {length:4d} {seven} {thirty}'.format(length=len(cardboard),
+seven_cardboard = int(len(cardboard))
+print('Cardboard: {length:4d} {seven} {thirty}'.format(length=seven_cardboard,
 	seven=int(len(cardboard)*0.7),
 	thirty=(((len(cardboard))-int(len(cardboard)*0.7)))))
 
-print('Glass: {length:8d} {seven} {thirty}'.format(length=len(glass),
+seven_glass = int(len(glass))
+print('Glass: {length:8d} {seven} {thirty}'.format(length=seven_glass,
 	seven=int(len(glass)*0.7),
 	thirty=(((len(glass))-int(len(glass)*0.7)))))
 
-print('Metal: {length:8d} {seven} {thirty}'.format(length=len(metal),
+seven_metal = int(len(metal))
+print('Metal: {length:8d} {seven} {thirty}'.format(length=seven_metal,
 	seven=int(len(metal)*0.7),
 	thirty=(((len(metal))-int(len(metal)*0.7)))))
 
-print('Paper: {length:8d} {seven} {thirty}'.format(length=len(paper),
+seven_paper = int(len(paper))
+print('Paper: {length:8d} {seven} {thirty}'.format(length=seven_paper,
 	seven=int(len(paper)*0.7),
 	thirty=(((len(paper))-int(len(paper)*0.7)))))
 
-print('Plastic: {length:6d} {seven} {thirty}'.format(length=len(plastic),
+seven_plastic = int(len(plastic))
+print('Plastic: {length:6d} {seven} {thirty}'.format(length=seven_plastic,
 	seven=int(len(plastic)*0.7),
 	thirty=(((len(plastic))-int(len(plastic)*0.7)))))
 
+
 CUR_DIRECTORY = os.path.abspath(os.getcwd())
 
-for _ in range(int(len(cardboard)*0.7)):
-	choice = random.choice(cardboard)
-	os.rename(CUR_DIRECTORY+'/dataset/cardboard/'+choice,
-		CUR_DIRECTORY+'/dataset/train/cardboard/'+choice)
-	cardboard = os.listdir('dataset/cardboard')
+if(len(os.listdir('dataset/train/cardboard')) != 192):
+	for _ in range(int(seven_cardboard*0.7)):
+		choice = random.choice(cardboard)
+		cardboard.remove(choice)
+		os.rename(CUR_DIRECTORY+'/dataset/cardboard/'+choice,
+			CUR_DIRECTORY+'/dataset/train/cardboard/'+choice)
+	print(len(os.listdir('dataset/train/cardboard')))
 
-# for _ in range(int(len(glass)*0.7)):
-# 	choice = random.choice(glass)
-# 	os.rename('/dataset/glass/'+choice+)
+if(len(os.listdir('dataset/train/glass')) != 178):
+	for _ in range(int(seven_glass*0.7)):
+		choice = random.choice(glass)
+		glass.remove(choice)
+		os.rename(CUR_DIRECTORY+'/dataset/glass/'+choice,
+		CUR_DIRECTORY+'/dataset/train/glass/'+choice)
+	print(len(os.listdir('dataset/train/glass')))
 
-# for _ in range(int(len(metal)*0.7)):
-# 	choice = random.choice(metal)
-# 	os.rename('/dataset/metal/'+choice+)
+if(len(os.listdir('dataset/train/metal')) != 144):
+	for _ in range(int(seven_metal*0.7)):
+		choice = random.choice(metal)
+		metal.remove(choice)
+		os.rename(CUR_DIRECTORY+'/dataset/metal/'+choice,
+		CUR_DIRECTORY+'/dataset/train/metal/'+choice)
+	print(len(os.listdir('dataset/train/metal')))
 
-# for _ in range(int(len(paper)*0.7)):
-# 	choice = random.choice(paper)
-# 	os.rename('/dataset/paper/'+choice+)
+if(len(os.listdir('dataset/train/paper')) != 196):
+	for _ in range(int(seven_paper*0.7)):
+		choice = random.choice(paper)
+		paper.remove(choice)
+		os.rename(CUR_DIRECTORY+'/dataset/paper/'+choice,
+		CUR_DIRECTORY+'/dataset/train/paper/'+choice)
+	print(len(os.listdir('dataset/train/paper')))
 
-# for _ in range(int(len(plastic)*0.7)):
-# 	choice = random.choice(plastic)
-# 	os.rename('/dataset/plastic/'+choice+)
+if(len(os.listdir('dataset/train/plastic')) != 340):
+	for _ in range(int(seven_plastic*0.7)):
+		choice = random.choice(plastic)
+		plastic.remove(choice)
+		os.rename(CUR_DIRECTORY+'/dataset/plastic/'+choice,
+		CUR_DIRECTORY+'/dataset/train/plastic/'+choice)
+	print(len(os.listdir('dataset/train/plastic')))
+
