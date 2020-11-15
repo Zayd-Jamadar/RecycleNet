5629baf5f2e6fa8a807f2ee409d26a4e922d0f6d
Harsh188 <hiharshith18@gmail.com>
2020-11-15 18:09:13 +0530

    changed loss function and last layers of the model

--- RecycleNet/custom_resnet.py
+++ RecycleNet/custom_resnet.py
@@ -1,4 +1,4 @@
-DATASET_DIR = './dataset'
+DATASET_DIR = './dataset/train'
 CHECKPOINT_PATH = './checkpoint/'
 EPOCHS = 10
 STEPS_PER_EPOCH = 30
@@ -10,6 +10,7 @@ import time
 from resnet50 import ResNet50
 
 import tensorflow as tf
+import tensorflow_addons as tfa
 from tensorflow import keras
 from keras.models import Model
 from keras.layers import Dense, Input, Activation, Flatten
@@ -21,8 +22,14 @@ image_input = Input(shape=(224, 224, 3))
 
 def train_model():
     model = ResNet50(input_tensor=image_input, include_top=True, weights='imagenet')
+    last_layer = model.get_layer('avg_pool').output
+    x = Flatten(name='flatten')(last_layer)
+    out = Dense(5, name='output_layer')(x)
 
-    model.summary()
+    custom_resnet_model = Model(inputs=image_input, outputs=out)
+
+    # custom_resnet_model = get_model()
+    custom_resnet_model.summary()
 
     # Comment these two lines before training
     # for layer in model.layers[:-1]:
@@ -44,11 +51,15 @@ def train_model():
         monitor='accuracy'
     )
 
-    model.compile(loss='hinge',
-                                optimizer='adadelta',
-                                metrics=['accuracy'], )
+    opt = tfa.optimizers.SGDW(learning_rate = 0.01, 
+                            weight_decay=0.0001,
+                            momentum=0.9)
 
-    history = model.fit(                    
+    custom_resnet_model.compile(loss='hinge',
+                                optimizer=opt,
+                                metrics=['accuracy'],)
+
+    history = custom_resnet_model.fit(                    
         train_generator,
         steps_per_epoch=STEPS_PER_EPOCH,
         epochs=EPOCHS,
@@ -63,4 +74,5 @@ def get_model():
 
 if __name__ == '__main__':
     train_model()
+    # get_model()
 
